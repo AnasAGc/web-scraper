f937db888d78bf75f442407d10a4482a0a46ab41
anas <civil.anas1000@gmail.com>
2021-08-17 18:08:18 +0300

    fix the test

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -7,7 +7,6 @@ def get_citations_needed_count(url):
     response  = requests.get(url)
     soup = BeautifulSoup(response.text,'html.parser')
     result=soup.find_all('sup',class_='noprint Inline-Template Template-Fact')
-   
     return f'{len(result)} citations needed'
     
 
